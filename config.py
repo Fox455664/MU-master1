553e028100210d4e9a4071ea384bdf82f78f1276
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2024-05-12 01:47:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Fox455664/MU-master1.git")
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "MU-master1")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FTTUTY")
