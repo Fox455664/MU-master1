df24c5d02ca4c375287c1cea1b1ffb74661db20d
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2024-05-14 14:22:44 +0300

    Update play.py

--- AnonX/utils/inline/play.py
+++ AnonX/utils/inline/play.py
@@ -66,7 +66,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="إضغط لاضافتي لمجموعتك ✅ ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"
+                text="إضغط لاضافتي لمجموعتك ✅ ", url=f"https://t.me/ BOT_USERNAME = "your_bot_username" "
             )
         ],
     ]
